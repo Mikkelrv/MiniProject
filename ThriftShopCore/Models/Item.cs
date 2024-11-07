53b4dff75986cdec76cdb3263732144c9553a721
MikRahVin <m.vintherr@gmail.com>
2024-11-07 11:54:44 +0100

    Added modelclasses to core

---
+++ ThriftShopCore/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace ThriftShopCore.Models
+
+{
+    public class Item
+    {
+        public required string Name { get; set; }
+        public required double Price { get; set; }
+        public required string Description { get; set; }
+        public required string ImageUrl { get; set; }
+        public required DateTime Listed { get; set; } = DateTime.Now;
+        public required Status Status { get; set; }
+        public required string SellerEmail { get; set; }
+        public DateTime? SoldTime { get; set; } 
+
+    }
+}
---
+++ ThriftShopCore/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThriftShopCore.Models
+{
+    public class User
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public List<Item> Purchases { get; set; } = new();
+        public List<Item> Selling { get; set; } = new();
+    }
+}
