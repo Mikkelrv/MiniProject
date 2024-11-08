6b5c21019d090ef27c2325e9a6a86bc4bb219330
MikRahVin <m.vintherr@gmail.com>
2024-11-08 12:00:04 +0100

    Added login component

---
+++ ThriftShopCore/Models/UserAuthentication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThriftShopCore.Models
+{
+    public class UserAuthentication
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength=8 , ErrorMessage = "The password must be more than 8 characters long and less than 50 ")]
+        public string Password { get; set; }
+    }
+}
