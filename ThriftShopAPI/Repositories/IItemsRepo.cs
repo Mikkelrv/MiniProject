2d7f09504140b7e918ecac1ff83021b94fe75db5
asgermunk <75023079+asgermunk@users.noreply.github.com>
2024-11-11 10:13:37 +0100

    Made the interface for the repo

--- ThriftShopAPI/Repositories/IItemsRepo.cs
+++ ThriftShopAPI/Repositories/IItemsRepo.cs
@@ -1,6 +1,12 @@
-﻿namespace ThriftShopAPI.Repositories
+﻿using ThriftShopCore.Models;
+namespace ThriftShopAPI.Repositories
 {
     public interface IItemsRepo
     {
+        void addItem(Item item);
+        void deleteItem(string id);
+        List<Item> getItems(Filter filter);
+        Item getItem(string id);
+        void updateItem(Item item);
     }
 }
--- ThriftShopAPI/Repositories/ItemsRepo.cs
+++ ThriftShopAPI/Repositories/ItemsRepo.cs
@@ -1,6 +1,32 @@
-﻿namespace ThriftShopAPI.Repositories
+﻿using ThriftShopCore.Models;
+
+namespace ThriftShopAPI.Repositories
 {
     public class ItemsRepo : IItemsRepo
     {
+        public void addItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteItem(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item getItem(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Item> getItems(Filter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
