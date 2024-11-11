2de43d1c5f3345ba79c9d27a354f8940facd3b2b
asgermunk <75023079+asgermunk@users.noreply.github.com>
2024-11-11 12:14:42 +0100

    Added functionality to ItemRepo

--- ThriftShopAPI/Repositories/ItemsRepo.cs
+++ ThriftShopAPI/Repositories/ItemsRepo.cs
@@ -34,17 +34,49 @@ namespace ThriftShopAPI.Repositories
 
         public Item getItem(ObjectId id)
         {
-            throw new NotImplementedException();
+            var filter = Builders<Item>.Filter.Eq(item => item._id, id);
+            return _collection.Find(filter).FirstOrDefault();
         }
 
         public List<Item> getItems(Filter filter)
         {
-            throw new NotImplementedException();
+            var minPriceFilter = Builders<Item>.Filter.Gte(item => item.Price, filter.MinPrice);
+            var maxPriceFilter = Builders<Item>.Filter.Lte(item => item.Price, filter.MaxPrice);
+            var categoryFilter = Builders<Item>.Filter.Eq(item => item.Category, filter.Category);
+            var statusFilter = Builders<Item>.Filter.Eq(item => item.Status, filter.Status);
+            var queryFilterName = Builders<Item>.Filter.Regex(item => item.Name, new BsonRegularExpression(filter.Query, "i"));
+            var queryFilterDescription = Builders<Item>.Filter.Regex(item => item.Description, new BsonRegularExpression(filter.Query, "i"));
+
+            var totalFilter = Builders<Item>.Filter.Empty;
+            if (filter.MinPrice != 0)
+            {
+                totalFilter = Builders<Item>.Filter.And(totalFilter, minPriceFilter);
+            }
+            if (filter.MaxPrice != 0) {
+                totalFilter = Builders<Item>.Filter.And(totalFilter, maxPriceFilter);
+            }
+            if (filter.Category != null)
+            {
+                totalFilter = Builders<Item>.Filter.And(totalFilter, categoryFilter);
+            }
+            if (filter.Status != null)
+            {
+                totalFilter = Builders<Item>.Filter.And(totalFilter, statusFilter);
+            }
+            if (filter.Query != null)
+            {
+                totalFilter = Builders<Item>.Filter.And(totalFilter, Builders<Item>.Filter.Or(queryFilterName, queryFilterDescription));
+            }
+
+            return _collection.Find(totalFilter).ToList();
+
+
         }
 
         public void updateItem(Item item)
         {
-            throw new NotImplementedException();
+            var filter = Builders<Item>.Filter.Eq(i => i._id, item._id);
+            _collection.ReplaceOne(filter, item);
         }
     }
 }
