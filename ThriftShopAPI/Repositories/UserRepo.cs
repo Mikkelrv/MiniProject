2c674215f7ee4b4a3df3541c968c09f7ab07ab7f
asgermunk <75023079+asgermunk@users.noreply.github.com>
2024-11-11 10:17:16 +0100

    Implemented the interface for the user

--- ThriftShopAPI/Repositories/UserRepo.cs
+++ ThriftShopAPI/Repositories/UserRepo.cs
@@ -1,6 +1,27 @@
-﻿namespace ThriftShopAPI.Repositories
+﻿using ThriftShopCore.Models;
+
+namespace ThriftShopAPI.Repositories
 {
     public class UserRepo : IUserRepo
     {
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUser(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
