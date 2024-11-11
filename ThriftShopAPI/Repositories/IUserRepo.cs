e24a64899c484520a8c553507db3ba45404d2136
asgermunk <75023079+asgermunk@users.noreply.github.com>
2024-11-11 10:16:04 +0100

    Added user interface

--- ThriftShopAPI/Repositories/IUserRepo.cs
+++ ThriftShopAPI/Repositories/IUserRepo.cs
@@ -1,6 +1,12 @@
-﻿namespace ThriftShopAPI.Repositories
+﻿using ThriftShopCore.Models;
+namespace ThriftShopAPI.Repositories
 {
     public interface IUserRepo
     {
+        User GetUser(string id);
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(string id);
+
     }
 }
