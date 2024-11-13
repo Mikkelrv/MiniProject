d2fdf579b40ae74f82b73154782639db7d231b8d
BovineFreedo <frankskorstengaard@gmail.com>
2024-11-13 15:55:20 +0100

    UNFINISHED, still creating a login form

--- ThriftShopAPI/Controllers/UserController.cs
+++ ThriftShopAPI/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using ThriftShopCore.Models;
 namespace ThriftShopAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
         private IUserRepo _userRepo;
